8b2f11103ea1edcb5b155796e40aeb9a903c53e7
Josh Watkins <joshwatkinssfs@gmail.com>
2023-03-01 16:05:08 -0800

    testing error checking

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const NODE_ENV = process.env.NODE_ENV
 
-var apiKey = process.env.GOOGLE_MAPS_API_KEY;
+var apiKey = NODE_ENV.GOOGLE_MAPS_API_KEY;
 var hostUrl = 'https://majestic-chimera-34ed15.netlify.app/';
 
 var marker;
