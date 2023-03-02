0c0dcf7b1e55514397d8fcb031fdcb586184f75f
Josh Watkins <joshwatkinssfs@gmail.com>
2023-03-01 16:59:31 -0800

    testing again

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var apiKey = 'AIzaSyCJrWz6gA0wql676OZAS1hVKlF7Cc38o_I';
-var hostUrl = 'https://majestic-chimera-34ed15.netlify.app/index.html';
+var hostUrl = 'https://cors-anywhere.herokuapp.com/corsdemo';
 
 var marker;
 var map;
