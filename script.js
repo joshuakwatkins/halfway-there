38aa84841ee74d1d2e6eb8507f81dd7cee8df819
Josh Watkins <joshwatkinssfs@gmail.com>
2023-03-01 18:34:04 -0800

    updated ui

--- script.js
+++ script.js
@@ -89,14 +89,9 @@ function calcRoute() {
         console.log(userType);
         var config = {
           method: 'get',
-          url: hostUrl + 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + midPoint.lat() + '%2C' + midPoint.lng() + '&opennow=true&radius=' + radius + '&keyword=' + userKeyword + '&key=' + apiKey,
+          url: hostUrl + 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + midPoint.lat() + '%2C' + midPoint.lng() + '&opennow=true&rankby=distance&keyword=' + userKeyword + '&key=' + apiKey,
           header: {  }
         }
-        // var config = {
-        //   method: 'get',
-        //   url: hostUrl + 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + midPoint.lat() + '%2C' + midPoint.lng() + '&opennow=true&radius=' + radius + '&type=' + userType + '&key=' + apiKey,
-        //   header: { }
-        // }
         
 
         fetch(config.url)
@@ -164,114 +159,6 @@ function calcRoute() {
     };
     console.log(markerSet);
   
-
-
-  var moviesAndTimes = ['.cinema1','.cinema2','.cinema3'];
-  var joshNeeds = []; //array containing 3 subarrays of lat,long
-  
-function doomsday() {
-  event.preventDefault();
-  var c = moment().format();
-  var geoArray = [-22, 14];  //use geoArray.push() to add to lat and long to this
-  var movieCoord = geoArray.toString().replace(',',';');
-  var cinArray = [];
-console.log(movieCoord);
- var settings = {
-    "url": "https://api-gate2.movieglu.com/cinemasNearby/?n=3",
-    "method": "GET",
-    "timeout": 0,
-    "headers": {
-    "api-version": "v200",
-    "Authorization": "Basic Q09ESV85X1hYOnYwWVJHZzRtMVdZMw==",  //change when official
-    "client": "CODI_9",
-    "x-api-key": "mF0IdpuMdd7g01GUbV0ozdb9cK5wJmmRZCT7Wph0",  //change when official
-    "device-datetime": c,   //use moment.js
-    "territory": "XX",      //should be US when official
-    "geolocation": movieCoord
-    },
-    };
-
-    $.ajax(settings).done(function (response) {
-    console.log(response);
-    for (i=0; i < 3; i++) {
-      var cinTarget = $(moviesAndTimes[i]);  // placeholder; MUST CHANGE TO SEE INFO ON PAGE!
-      
-    var cinAID = response.cinemas[i].cinema_id;
-
-    var cinemaName = document.createElement("h3");
-    cinemaName.textContent = response.cinemas[i].cinema_name;
-
-    var cinemaAddress = document.createElement("p");
-    cinemaAddress.textContent ="Address: " + response.cinemas[i].address;
-
-    var cinFilm = document.createElement("h5");
-      cinFilm.textContent ="Popular Movies: ";
-
-    var navEl = [];
-    navEl.push(response.cinemas[i].lat);
-    navEl.push(response.cinemas[i].lng);
-
-     cinTarget.append(cinemaName, cinemaAddress, cinFilm);
-
-     cinArray.push(cinAID);
-     joshNeeds.push(navEl);
-    }
-    acquireShowTimes();
-    });
-
-    function acquireShowTimes() {
-
-      var needDate = moment().format("YYYY-MM-DD");
-      for (let i = 0; i < cinArray.length; i++) {
-        var settings = {
-          url:
-            "https://api-gate2.movieglu.com/cinemaShowTimes/?&cinema_id=" +
-            cinArray[i] +
-            "&date=" +
-            needDate +
-            "&sort=popularity",
-          method: "GET",
-          timeout: 0,
-          headers: {
-            "api-version": "v200",
-            Authorization: "Basic Q09ESV85X1hYOnYwWVJHZzRtMVdZMw==", //change when official
-            client: "CODI_9",
-            "x-api-key": "mF0IdpuMdd7g01GUbV0ozdb9cK5wJmmRZCT7Wph0", //change when official
-            "device-datetime": c, //use moment.js
-            territory: "XX", //should be US when official
-            geolocation: movieCoord,
-          },
-        };
-
-        $.ajax(settings).done(function (response) {
-          var cinTarget = $(moviesAndTimes[i]);
-         var listy = document.createElement('ol');
-         cinTarget.append(listy);
-          for (let j = 0; j < 4; j++) {
-              var sTime = [];
-     
-
-            for (
-              let b = 0;
-              b < 3;
-              b++
-            ) {
-
-              sTime.push(response.films[j].showings.Standard.times[b].start_time);
-            }
-            var cinTime = document.createElement("li");
-            cinTime.textContent = response.films[j].film_name + " -\n Start Times: " + sTime;
-            listy.append(cinTime);
-            
-          }
-
-        });
-      }
-     
-      } }
-
-
-
 
 
 // Script to open and close sidebar
